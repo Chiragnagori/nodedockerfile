ae5fe47933c8156b3355f695e8bd21bb9a5c15e2
root <root@chiragvm.adtelddgriwepl5wao3ee00kvh.bx.internal.cloudapp.net>
2023-07-11 10:32:03 +0000

    update index

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 
 const app = express()
 
-
+const port = process.env.PORT || 3000;
 
 
 app.get('/', (req, res) => res.send('Hello from CHIRAG node image!'))
 
-app.listen(3000, () => console.log('Server ready'))
+app.listen(PORT, () => console.log('Server ready'))
